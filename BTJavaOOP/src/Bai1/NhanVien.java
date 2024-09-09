e06107a3b6a619eb4926c46bed0da4de8ad5fd1a
TNGGG <112959173+TNGGG@users.noreply.github.com>
2024-09-09 22:37:53 +0700

    Add files via upload

---
+++ BTJavaOOP/src/Bai1/NhanVien.java
@@ -0,0 +1,12 @@
+package Bai1;
+
+public class NhanVien {
+    public static String Ten;
+    public static int Tuoi;
+    public static void main(String[] args) {
+
+        NhanVien NhanVien=new NhanVien();
+        NhanVien.Ten="Nem";
+        NhanVien.Tuoi=25;
+    }
+}
---
+++ BTJavaOOP/src/Bai1/ThongTin.java
@@ -0,0 +1,8 @@
+package Bai1;
+
+public class ThongTin {
+    public static void main(String[] args) {
+        System.out.println("Ten nhan viên: "+NhanVien.Ten);
+        System.out.println("Tuổi nhan viên: "+NhanVien.Tuoi);
+    }
+}
